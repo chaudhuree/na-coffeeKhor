22e98740c5b007d68dd7a127740eaec6adc00d8c
chaudhuree <chaudhuree00@gmail.com>
2022-03-11 02:45:11 +0600

    design banner component

--- components/banner.js
+++ components/banner.js
@@ -1,9 +1,17 @@
-const Banner = ({buttonText,buttonClickHandler}) => {
+import styles from "./Banner.module.css";
+const Banner = ({ buttonText, buttonClickHandler }) => {
   return (
-    <div>
-      <h1>coffee khor</h1>
-      <p>discover coffee shop near you!</p>
-      <button onClick={buttonClickHandler}>{buttonText}</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>coffee</span>{" "}
+        <span className={styles.title2}>khor</span>{" "}
+      </h1>
+      <p className={styles.subTitle}>discover coffee shop near you!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={buttonClickHandler}>
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 };
