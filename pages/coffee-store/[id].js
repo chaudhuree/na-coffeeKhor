d40af9576cb9ac7418dee2503d2f4e633c6ea52e
chaudhuree <chaudhuree00@gmail.com>
2022-03-14 07:56:33 +0600

    docs: tested link property to use it we need to import it it is similar to a tag

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
 function CoffeeStore() {
   const route = useRouter();
   console.log(route);
-  return <div>CoffeeStore id: {route.query.id}</div>;
+  return <div>CoffeeStore id: {route.query.id}
+  <div></div>
+  <Link href='/'><a>Back to Home</a></Link>
+  </div>;
 }
 
 export default CoffeeStore;
