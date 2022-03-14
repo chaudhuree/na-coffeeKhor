fd842a213cc3bf215687892f2583a699856b0762
chaudhuree <chaudhuree00@gmail.com>
2022-03-14 07:06:22 +0600

    docs: added head tag next js has a head component. we have to import it form next/head and then use it. it makes it seo effecient

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>coffee khor ☕☕</title>
+        <title>coffee khor ☕</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
