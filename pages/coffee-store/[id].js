7c845f130c050c9a18c73abec364d54a346c6e4d
chaudhuree <chaudhuree00@gmail.com>
2022-03-15 02:29:38 +0600

    nothing important

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -5,11 +5,19 @@ import React from "react";
 function CoffeeStore() {
   const route = useRouter();
   console.log(route);
-  return <div>CoffeeStore id: {route.query.id}
-  <div></div>
-  <Link href='/'><a>Back to Home</a></Link> <br />
-  <Link href='/coffee-store/dynamic'><a>redirect to dynamic</a></Link>
-  </div>;
+  return (
+    <div>
+      CoffeeStore id: {route.query.id}
+      <div></div>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>{" "}
+      <br />
+      <Link href="/coffee-store/dynamic">
+        <a>redirect to dynamic</a>
+      </Link>
+    </div>
+  );
 }
 
 export default CoffeeStore;
