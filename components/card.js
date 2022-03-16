df6121e4563b9461c124ac3852d79a8826973ac4
chaudhuree <chaudhuree00@gmail.com>
2022-03-17 04:19:21 +0600

    docs: added classnames install by command npm install classnames use it in card component

--- components/card.js
+++ components/card.js
@@ -1,3 +1,4 @@
+import cls from 'classnames';
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./card.module.css";
@@ -7,7 +8,7 @@ const Card = (props) => {
   return (
     <Link href={props.href}>
       <a className={styles.cardLink}>
-        <div className={styles.container}>
+        <div className={cls('glass', styles.container)}>
           <div className={styles.cardHeaderWrapper}>
             <h2 className={styles.cardHeader}>{props.name}</h2>
           </div>
