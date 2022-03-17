c7754983893d6dd10d2754ec1f98b939304cf022
chaudhuree <chaudhuree00@gmail.com>
2022-03-17 21:58:24 +0600

    fix: error to parse image
    have to add domain to next.config.js

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,28 @@ import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/banner";
 import Card from "../components/card";
+import CoffeeStore from "../data/coffee-stores.json";
 import styles from "../styles/Home.module.css";
 
+
 export default function Home() {
-    const buttonClickHandler =()=>{
-        console.log('hello')
-    }
+  const buttonClickHandler = () => {
+    console.log("hello");
+  };
   return (
     <div className={styles.container}>
       <Head>
         <title>coffee khor ☕</title>
 
-        
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-
-      <Banner buttonText='view stores nearby' buttonClickHandler={buttonClickHandler} />
-      <div className={styles.heroImage}>
+        <Banner
+          buttonText="view stores nearby"
+          buttonClickHandler={buttonClickHandler}
+        />
+        <div className={styles.heroImage}>
           <Image
             src="/../public/hero-image.png"
             width={700}
@@ -29,37 +32,17 @@ export default function Home() {
           />
         </div>
         <div className={styles.cardLayout}>
-              
-                <Card
-                  
-                  className={styles.card}
-                  name='DarkHorse'
-                  imgUrl='/../public/static/darkHorse.jpeg'
-                  href='/coffee-store/darkhorse-coffee'
-                />
-              
-              
-                <Card
-                  
-                  className={styles.card}
-                  name='DarkHorse'
-                  imgUrl='/../public/static/darkHorse.jpeg'
-                  href='/coffee-store/darkhorse-coffee'
-                />
-              
-              
-                <Card
-                  
-                  className={styles.card}
-                  name='DarkHorse'
-                  imgUrl='/../public/static/darkHorse.jpeg'
-                  href='/coffee-store/darkhorse-coffee'
-                />
-              
-            </div>
+          {CoffeeStore.map((data) => {
+            return <Card
+              className={styles.card}
+              key={data.id}
+              name={data.name}
+              imgUrl={data.imgUrl}
+              href={`/coffee-store/${data.id}`}
+            />;
+          })}
+        </div>
       </main>
-
-
     </div>
   );
 }
