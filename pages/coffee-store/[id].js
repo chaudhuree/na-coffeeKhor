38eebb5d31f6a9d9ba5f11becd8a86842dfb6216
chaudhuree <chaudhuree00@gmail.com>
2022-03-22 02:21:06 +0600

    docs: added styles for col 1

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import CoffeeStoreData from "../../data/coffee-stores.json";
+import styles from "../../styles/coffeestore.module.css";
 
 export function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -14,14 +17,14 @@ export function getStaticProps(staticProps) {
   };
 }
 export function getStaticPaths() {
-  const paths=CoffeeStoreData.map(coffeestore=>{
-    return {params:{id:coffeestore.id.toString()}}
-  })
+  const paths = CoffeeStoreData.map((coffeestore) => {
+    return { params: { id: coffeestore.id.toString() } };
+  });
   return {
     // paths: [
     //   { params: { id: "0" } },
     //   { params: { id: "1" } }
-      
+
     // ],
     paths,
     fallback: true, // false or 'blocking'
@@ -30,24 +33,44 @@ export function getStaticPaths() {
 function CoffeeStore(props) {
   const route = useRouter();
   // if id is not in the getStaticPaths
-  if(route.isFallback){
-    return <div>loading....</div>
+  if (route.isFallback) {
+    return <div>loading....</div>;
   }
-  const {address,name,neighbourhood}= props.CoffeeStore
+  const { address, name, neighbourhood, imgUrl } = props.CoffeeStore;
   return (
-    <div>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
       {/* CoffeeStore id: {route.query.id}
       <div></div> */}
-      <Link href="/">
-        <a>Back to Home</a>
-      </Link>
-      <br />
-      {/* <Link href="/dynamic">
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>← Back to Home</a>
+            </Link>
+          </div>
+          <br />
+          {/* <Link href="/dynamic">
         <a>redirect to dynamic</a>
       </Link> */}
-      <p>{name}</p>
-      <p>{address}</p>
-      <p>{neighbourhood}</p>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            alt={name}
+            width={600}
+            height={300}
+            className={styles.storeImg}
+          />
+        </div>
+        <div className={styles.col2}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+        </div>
+      </div>
     </div>
   );
 }
