22bdb76770c8a0feb640d44f120b6b8885472c75
chaudhuree <chaudhuree00@gmail.com>
2022-04-11 13:19:00 +0600

    docs: set up env.local variable

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,14 @@ import styles from "../styles/Home.module.css";
 export async function getStaticProps(context) {
   // fetch data from foursquare
   const response = await fetch(
-    "https://api.foursquare.com/v3/places/nearby?ll=43.65267326999575,-79.39545615725015&query=coffee stores&v=20220105&limit=5",
+    "https://api.foursquare.com/v3/places/nearby?ll=43.65267326999575,-79.39545615725015&query=coffee stores&v=20220105&limit=7",
     {
       headers: {
-        Authorization: "fsq3CI/wq0x2IvwqZNc/a6+i9pf9RlYueY5QTgk8uRPEMbA=",
+        Authorization: process.env.NEXT_PUBLIC_FOURSQUARE_KEY},
       },
-    }
-  );
+  )
+ 
+
 
   const data = await response.json();
   CoffeeStoreData = data?.results;
@@ -52,13 +53,16 @@ export default function Home(props) {
         </div>
         <div className={styles.cardLayout}>
           {props.CoffeeStore?.map((data) => {
-            const {fsq_id,name,location} = data;
+            const { fsq_id, name, location } = data;
             return (
               <Card
                 className={styles.card}
                 key={fsq_id}
                 name={name}
-                imgUrl={data.imgUrl ||"https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"}
+                imgUrl={
+                  data.imgUrl ||
+                  "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"
+                }
                 href={`/coffee-store/${fsq_id}`}
               />
             );
