48a5017d00ac43cd8ec6319e7c5f11aa5d9f346b
chaudhuree <chaudhuree00@gmail.com>
2022-04-14 01:05:08 +0600

    fix:issus solved for problem Unhandled Runtime Error Error: Failed to load static props

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -10,11 +10,17 @@ import styles from "../../styles/coffeestore.module.css";
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
   const CoffeeStoresData = await CoffeeStores();
+  let data = null;
+  try {
+    data = CoffeeStoresData.find((CoffeeStore) => {
+      return CoffeeStore.fsq_id.toString() === params.id; // params.id is the id from the url which is always a string
+    })
+  } catch (err) {
+    console.log(err.message)
+  };
   return {
     props: {
-      CoffeeStore: CoffeeStoresData.find((CoffeeStore) => {
-        return CoffeeStore.fsq_id.toString() === params.id; // params.id is the id from the url which is always a string
-      }),
+      CoffeeStore: data ? data : {}
     },
   };
 }
@@ -37,66 +43,72 @@ const handleUpvoteButton = () => {
   console.log("upvotebutton");
 };
 function CoffeeStore(props) {
+  console.log(props)
   const route = useRouter();
   // if id is not in the getStaticPaths
   if (route.isFallback) {
     return <div>loading....</div>;
   }
-  const { location, name, imgUrl } = props.CoffeeStore;
-  return (
-    <div className={styles.layout}>
-      <Head>
-        <title>{name}</title>
-      </Head>
-      {/* CoffeeStore id: {route.query.id}
-      <div></div> */}
-      <div className={styles.container}>
-        <div className={styles.col1}>
-          <div className={styles.backToHomeLink}>
-            <Link href="/">
-              <a>← Back to Home</a>
-            </Link>
-          </div>
+  const { address, name, imgUrl,country } = props.CoffeeStore;
+  if (props.CoffeeStore) {
+    return (
 
-          {/* <Link href="/dynamic">
+      <div className={styles.layout}>
+        <Head>
+          <title>{name}</title>
+        </Head>
+        {/* CoffeeStore id: {route.query.id}
+      <div></div> */}
+        <div className={styles.container}>
+          <div className={styles.col1}>
+            <div className={styles.backToHomeLink}>
+              <Link href="/">
+                <a>← Back to Home</a>
+              </Link>
+            </div>
+
+            {/* <Link href="/dynamic">
         <a>redirect to dynamic</a>
       </Link> */}
-          <div className={styles.nameWrapper}>
-            <h1 className={styles.name}>{name}</h1>
-          </div>
-          <Image
-            src={
-              imgUrl ||
-              "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"
-            }
-            alt={name}
-            width={600}
-            height={300}
-            className={styles.storeImg}
-          />
-        </div>
-        <div className={cls("glass", styles.col2)}>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width="24" height="24" />
-            <p className={styles.text}>{location.address}</p>
-          </div>
-          {location.neighborhood && (
-            <div className={styles.iconWrapper}>
-              <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-              <p className={styles.text}>{location.neighborhood}</p>
+            <div className={styles.nameWrapper}>
+              <h1 className={styles.name}>{name}</h1>
             </div>
-          )}
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/star.svg" width="24" height="24" />
-            <p className={styles.text}>1</p>
+            <Image
+              src={
+                imgUrl ||
+                "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80"
+              }
+              alt={name}
+              width={600}
+              height={300}
+              className={styles.storeImg}
+            />
           </div>
-          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
-            Up Vote!
-          </button>
+          <div className={cls("glass", styles.col2)}>
+         { address && (<div className={styles.iconWrapper}>
+              <Image alt="coffeeStores" src="/static/icons/places.svg" width="24" height="24" />
+              <p className={styles.text}>{address}</p>
+            </div>)}
+            {country && (
+              <div className={styles.iconWrapper}>
+                <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="coffeeStores" />
+                <p className={styles.text}>{country}</p>
+              </div>
+            )}
+            <div className={styles.iconWrapper}>
+              <Image alt="coffeeStores" src="/static/icons/star.svg" width="24" height="24" />
+              <p className={styles.text}>1</p>
+            </div>
+            <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+              Up Vote!
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <div><p>not found</p></div>
+  }
 }
 
 export default CoffeeStore;
