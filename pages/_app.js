ec1f66077a857caa5b270d6db34c3e4aa3101b78
chaudhuree <chaudhuree00@gmail.com>
2022-04-17 04:28:49 +0600

    docs: initial context setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
+import { createContext } from "react";
 import "../styles/globals.css";
+
+const StoreContext = createContext();
+
+const StoreProvider=({children})=>{
+const initialState={
+  latLong:"",
+  CoffeeStores:[],
+}
+// must return never forget to write return
+  return (<StoreContext.Provider value={{state:initialState}}>
+    {children}
+  </StoreContext.Provider>)
+} 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <StoreProvider>
       <Component {...pageProps} />
+      </StoreProvider>
     </>
   );
 }
