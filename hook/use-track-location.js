853b049e93055e6562afc864e29bbf65967dc1f8
chaudhuree <chaudhuree00@gmail.com>
2022-04-17 05:37:31 +0600

    docs: dispatch set lat long

--- hook/use-track-location.js
+++ hook/use-track-location.js
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ACTION_TYPES, StoreContext } from "../pages/_app";
 const useTrackLocation = () => {
+const { dispatch,state} = useContext(StoreContext)
+
   const [locationErrorMsg, setLocationErrorMsg] = useState("");
-  const [latLong, setLatLong] = useState("");
+  // const [latLong, setLatLong] = useState("");
   const [isFindingLocation, setIsFindingLocation] = useState(false);
   const success = (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     setLatLong(`${latitude},${longitude}`);
+    dispatch({ type: ACTION_TYPES.SET_LAT_LONG, payload: { latLong: `${latitude},${longitude}` } })
     setLocationErrorMsg("");
     setIsFindingLocation(false);
   };
@@ -27,7 +31,7 @@ const useTrackLocation = () => {
     }
   };
   return {
-    latLong,
+    // latLong,
     handleTrackLocation,
     locationErrorMsg,
     isFindingLocation,
