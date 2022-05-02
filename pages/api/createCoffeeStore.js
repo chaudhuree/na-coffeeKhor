f702d45aa99487d88e0d8913b6901ce2376523c2
chaudhuree <chaudhuree00@gmail.com>
2022-05-02 06:30:45 +0600

    docs: find mode is coded for airtable

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,20 @@
 const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_KEY);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
 
-const table=base('Table 1');
+const table = base('Table 1');
+console.log({ table });
+
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+    const findCoffeeStoreRecord = await table.select({
+      filterByFormula: `id="1"`,
+    }).firstPage()
+    console.log({findCoffeeStoreRecord});
+    if (findCoffeeStoreRecord.length != 0) {
+      res.json(findCoffeeStoreRecord)
+    } else {
+      res.json({ message: 'create database' })
+    }
+  }
+}
+export default createCoffeeStore;
