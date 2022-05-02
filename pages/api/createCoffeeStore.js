ccfd3ceefcadaad788e7c92d1851b19ff8065c5c
chaudhuree <chaudhuree00@gmail.com>
2022-05-02 06:38:08 +0600

    docs: wrap in try catch block

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -6,19 +6,26 @@ console.log({ table });
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
-    const findCoffeeStoreRecord = await table.select({
-      filterByFormula: `id="1"`,
-    }).firstPage()
-    console.log({findCoffeeStoreRecord});
-    if (findCoffeeStoreRecord.length != 0) {
-      const record=findCoffeeStoreRecord.map(record=>{
-        return {
-          ...record.fields
-        }
-      })
-      res.json(record)
-    } else {
-      res.json({ message: 'create database' })
+    try {
+      const findCoffeeStoreRecord = await table.select({
+        filterByFormula: `id="1"`,
+      }).firstPage()
+      console.log({ findCoffeeStoreRecord });
+      if (findCoffeeStoreRecord.length != 0) {
+        const record = findCoffeeStoreRecord.map(record => {
+          return {
+            ...record.fields
+          }
+        })
+        res.json(record)
+      } else {
+        res.json({ message: 'create database' })
+      }
+    }
+    catch (err) {
+      console.log('airtable data error:', err);
+      res.status(500)
+      res.json({ message: "something is wrong with airtable ", err })
     }
   }
 }
