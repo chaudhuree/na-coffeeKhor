3d8a62fda88a14b1210774e36f5539fa2e9df8e8
chaudhuree <chaudhuree00@gmail.com>
2022-05-10 20:17:01 +0600

    docs: api error handing if id or name is missing then do this

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,10 +5,10 @@ const table = base('Table 1');
 console.log({ table });
 
 const createCoffeeStore = async (req, res) => {
-  const {id,name,address,imgUrl,voting,country}=req.body;
+  const { id, name, address, imgUrl, voting, country } = req.body;
   if (req.method === "POST") {
     try {
-      const findCoffeeStoreRecord = await table.select({
+      if(id){const findCoffeeStoreRecord = await table.select({
         filterByFormula: `id="${id}"`,
       }).firstPage()
       console.log({ findCoffeeStoreRecord });
@@ -21,41 +21,50 @@ const createCoffeeStore = async (req, res) => {
         res.status(200)
         res.json(record)
       } else {
-        
 
 
+        if (id && name) {
 
-        // 
-       const createRecords= await table.create([
-          {
-            "fields": {
-              // "id": "3",
-              // "name": "juicy coffee store",
-              // "address": "my address",
-              // "imgUrl": "https://img1.com",
-              // "voting": 20,
-              // "country": "BD",
-              id,
-              name,
-              address,
-              imgUrl,
-              voting,
-              country,
+          // 
+          const createRecords = await table.create([
+            {
+              "fields": {
+                // "id": "3",
+                // "name": "juicy coffee store",
+                // "address": "my address",
+                // "imgUrl": "https://img1.com",
+                // "voting": 20,
+                // "country": "BD",
+                id,
+                name,
+                address,
+                imgUrl,
+                voting,
+                country,
+              }
             }
-          }
-        ])
-        const record = createRecords.map(record => {
-          return {
-            ...record.fields
-          }})
-        // 
-        res.json({ message: 'created database', record })
+          ])
+          const record = createRecords.map(record => {
+            return {
+              ...record.fields
+            }
+          })
+          // 
+          res.json({ message: 'created database', record })
+        }else{
+          res.json({message:"id or name is missing"})
+          res.status(500)
+        }
+      }}else{
+        res.status(400)
+        res.json({message:'id is missing'})
       }
+      
     }
     catch (err) {
-      console.log('airtable data error:', err);
+      console.log('error creating or finding a store:', err);
       res.status(500)
-      res.json({ message: "something is wrong with airtable ", err })
+      res.json({ message: 'error creating or finding a store:', err })
     }
   }
 }
