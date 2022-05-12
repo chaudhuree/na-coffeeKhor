505972591cb5b9666d1fff325c57a88cfe9bf045
chaudhuree <chaudhuree00@gmail.com>
2022-05-12 20:36:07 +0600

    docs: getCoffeeStoreById api validation

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,11 +1,36 @@
-const getCoffeeStoreById =(req,res)=>{
-    const {id}=req.query;
-    try {
-      res.json({message:`id is created ${id}`})
-    } catch (error) {
-      res.status(500)
-      res.json({message:'error to get data from api',error})
+const Airtable = require('airtable');
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_KEY);
+
+const table = base('Table 1');
+const getCoffeeStoreById = async (req, res) => {
+  const { id } = req.query;
+  try {
+    // try to find coffeeStore with specific id
+    if (id) {
+      const findCoffeeStoreRecord = await table.select({
+        filterByFormula: `id="${id}"`,
+      }).firstPage()
+      console.log({ findCoffeeStoreRecord });
+      // if coffeeStore is available 
+      if (findCoffeeStoreRecord.length != 0) {
+        const record = findCoffeeStoreRecord.map(record => {
+          return {
+            ...record.fields
+          }
+        })
+        res.status(200)
+        res.json(record)
+        // if coffeestore is not available then
+      }
+      else {
+        res.status(500)
+        res.json({ message: 'id is not found' })
+      }
     }
+  } catch (error) {
+    res.status(500)
+    res.json({ message: 'error to get data from api', error })
+  }
 }
 
 export default getCoffeeStoreById;
